cd34653dbbbb9663fc1539070db4a0c417a70d23
Anton Mihalev <antonmv@protonmail.com>
2017-11-21 19:04:52 +0300

    Update bundled pixivpy to latest.
    New version: ddf8489d33f669be983cdff4e65973445ec550da.

--- pixivpy3/__init__.py
+++ pixivpy3/__init__.py
@@ -1,9 +1,10 @@
 """
 Pixiv API library
 """
-__version__ = '3.0.1'
+__version__ = '3.3.3'
 
-from .api import PixivAPI
+from .papi import PixivAPI
+from .aapi import AppPixivAPI
 from .utils import PixivError
 
-__all__ = ("PixivAPI", "PixivError")
+__all__ = ("PixivAPI", "AppPixivAPI", "PixivError")
--- pixivpy3/api.py
+++ pixivpy3/api.py
@@ -1,221 +1,121 @@
 # -*- coding:utf-8 -*-
 
+import os
+import sys
+import shutil
 import json
 import requests
 
-from utils import PixivError, JsonDict
+from .utils import PixivError, JsonDict
 
-class BasePixivAPI:
-	access_token = None
-	user_id = 0
 
-	def parse_json(self, json_str):
-		"""parse str into JsonDict"""
+class BasePixivAPI(object):
+    client_id = 'bYGKuGVw91e0NMfPGp44euvGt59s'
+    client_secret = 'HP3RmkgAmEGro0gn1x9ioawQE8WMfvLXDz3ZqxpK'
 
-		def _obj_hook(pairs):
-			"""convert json object to python object"""
-			o = JsonDict()
-			for k, v in pairs.iteritems():
-				o[str(k)] = v
-			return o
+    access_token = None
+    user_id = 0
+    refresh_token = None
 
-		return json.loads(json_str, object_hook=_obj_hook)
+    def __init__(self, **requests_kwargs):
+        """initialize requests kwargs if need be"""
+        self.requests = requests.Session()
+        self.requests_kwargs = requests_kwargs
 
-	def require_auth(self):
-		if (self.access_token == None):
-			raise PixivError('Authentication required! Call login() or set_auth() first!')
+    def parse_json(self, json_str):
+        """parse str into JsonDict"""
 
-	def requests_call(self, method, url, headers={}, params=None, data=None):
-		""" requests http/https call for Pixiv API """
+        def _obj_hook(pairs):
+            """convert json object to python object"""
+            o = JsonDict()
+            for k, v in pairs.items():
+                o[str(k)] = v
+            return o
 
-		req_header = {
-			'Referer': 'http://spapi.pixiv.net/',
-			'User-Agent': 'PixivIOSApp/5.8.0',
-		}
-		# override use user headers
-		for k,v in headers.items():
-			req_header[k] = v
+        return json.loads(json_str, object_hook=_obj_hook)
 
-		try:
-			if (method == 'GET'):
-				return requests.get(url, params=params, headers=req_header)
-			elif (method == 'POST'):
-				return requests.post(url, params=params, data=data, headers=req_header)
-		except Exception, e:
-			raise PixivError('requests %s %s error: %s' % (method, url, e))
+    def require_auth(self):
+        if self.access_token is None:
+            raise PixivError('Authentication required! Call login() or set_auth() first!')
 
-		raise PixivError('Unknow method: %s' % method)
+    def requests_call(self, method, url, headers={}, params=None, data=None, stream=False):
+        """ requests http/https call for Pixiv API """
+        try:
+            if (method == 'GET'):
+                return self.requests.get(url, params=params, headers=headers, stream=stream, **self.requests_kwargs)
+            elif (method == 'POST'):
+                return self.requests.post(url, params=params, data=data, headers=headers, stream=stream, **self.requests_kwargs)
+            elif (method == 'DELETE'):
+                return self.requests.delete(url, params=params, data=data, headers=headers, stream=stream, **self.requests_kwargs)
+        except Exception as e:
+            raise PixivError('requests %s %s error: %s' % (method, url, e))
 
-	def set_auth(self, access_token, session_id=None):
-		self.access_token = access_token
-		#self.session = session_id
+        raise PixivError('Unknow method: %s' % method)
 
-	def login(self, username, password):
-		url = 'https://oauth.secure.pixiv.net/auth/token'
-		headers = {
-			'Referer': 'http://www.pixiv.net/',
-		}
-		data = {
-			'username': username,
-			'password': password,
-			'grant_type': 'password',
-			'client_id': 'bYGKuGVw91e0NMfPGp44euvGt59s',
-			'client_secret': 'HP3RmkgAmEGro0gn1x9ioawQE8WMfvLXDz3ZqxpK',
-		}
+    def set_auth(self, access_token, refresh_token=None):
+        self.access_token = access_token
+        self.refresh_token = refresh_token
 
-		r = self.requests_call('POST', url, headers=headers, data=data)
-		if (not r.status_code in [200, 301, 302]):
-			raise PixivError('[ERROR] login() failed! check username and password.\nHTTP %s: %s' % (r.status_code, r.text), header=r.headers, body=r.text)
+    def login(self, username, password):
+        return self.auth(username=username, password=password)
 
-		token = None
-		try:
-			# get access_token
-			token = self.parse_json(r.text)
-			self.access_token = token.response.access_token
-			self.user_id = token.response.user.id
-			print "AccessToken:", self.access_token
+    def auth(self, username=None, password=None, refresh_token=None):
+        """Login with password, or use the refresh_token to acquire a new bearer token"""
 
-		except:
-			raise PixivError('Get access_token error! Response: %s' % (token), header=r.headers, body=r.text)
+        url = 'https://oauth.secure.pixiv.net/auth/token'
+        headers = {
+            'App-OS': 'ios',
+            'App-OS-Version': '10.3.1',
+            'App-Version': '6.7.1',
+            'User-Agent': 'PixivIOSApp/6.7.1 (iOS 10.3.1; iPhone8,1)',
+        }
+        data = {
+            'get_secure_url': 1,
+            'client_id': self.client_id,
+            'client_secret': self.client_secret,
+        }
 
-		# return auth/token response
-		return token
+        if (username is not None) and (password is not None):
+            data['grant_type'] = 'password'
+            data['username'] = username
+            data['password'] = password
+        elif (refresh_token is not None) or (self.refresh_token is not None):
+            data['grant_type'] = 'refresh_token'
+            data['refresh_token'] = refresh_token or self.refresh_token
+        else:
+            raise PixivError('[ERROR] auth() but no password or refresh_token is set.')
 
-## Public-API
-class PixivAPI(BasePixivAPI):
+        r = self.requests_call('POST', url, headers=headers, data=data)
+        if (r.status_code not in [200, 301, 302]):
+            if data['grant_type'] == 'password':
+                raise PixivError('[ERROR] auth() failed! check username and password.\nHTTP %s: %s' % (r.status_code, r.text), header=r.headers, body=r.text)
+            else:
+                raise PixivError('[ERROR] auth() failed! check refresh_token.\nHTTP %s: %s' % (r.status_code, r.text), header=r.headers, body=r.text)
 
-	# Check auth and add BearerToken to headers
-	def auth_requests_call(self, method, url, headers={}, params=None, data=None):
-		self.require_auth()
-		headers['Authorization'] = 'Bearer %s' % self.access_token
-		return self.requests_call(method, url, headers, params, data)
+        token = None
+        try:
+            # get access_token
+            token = self.parse_json(r.text)
+            self.access_token = token.response.access_token
+            self.user_id = token.response.user.id
+            self.refresh_token = token.response.refresh_token
+        except:
+            raise PixivError('Get access_token error! Response: %s' % (token), header=r.headers, body=r.text)
 
-	def parse_result(self, req):
-		try:
-			return self.parse_json(req.text)
-		except Exception, e:
-			raise PixivError("parse_json() error: %s" % (e), header=req.headers, body=req.text)
+        # return auth/token response
+        return token
 
-	def bad_words(self):
-		url = 'https://public-api.secure.pixiv.net/v1.1/bad_words.json'
-		r = self.auth_requests_call('GET', url)
-		return self.parse_result(r)
+    def download(self, url, prefix='', path=os.path.curdir, name=None, replace=False, referer='https://app-api.pixiv.net/'):
+        """Download image to file (use 6.0 app-api)"""
+        if not name:
+            name = prefix + os.path.basename(url)
+        else:
+            name = prefix + name
 
-	# 作品详细
-	def works(self, illust_id):
-		url = 'https://public-api.secure.pixiv.net/v1/works/%d.json' % (illust_id)
-		params = {
-			'profile_image_sizes': 'px_170x170,px_50x50',
-			'image_sizes': 'px_128x128,small,medium,large,px_480mw',
-			'include_stats': 'true',
-		}
-		r = self.auth_requests_call('GET', url, params=params)
-		return self.parse_result(r)
-
-	# 用户资料
-	def users(self, author_id):
-		url = 'https://public-api.secure.pixiv.net/v1/users/%d.json' % (author_id)
-		params = {
-			'profile_image_sizes': 'px_170x170,px_50x50',
-			'image_sizes': 'px_128x128,small,medium,large,px_480mw',
-			'include_stats': 1,
-			'include_profile': 1,
-			'include_workspace': 1,
-			'include_contacts': 1,
-		}
-		r = self.auth_requests_call('GET', url, params=params)
-		return self.parse_result(r)
-
-	# 我的订阅
-	def me_feeds(self, show_r18=1):
-		url = 'https://public-api.secure.pixiv.net/v1/me/feeds.json'
-		params = {
-			'relation': 'all',
-			'type': 'touch_nottext',
-			'show_r18': show_r18,
-		}
-		r = self.auth_requests_call('GET', url, params=params)
-		return self.parse_result(r)
-
-	# 用户作品列表
-	# publicity:  public, private
-	def users_works(self, author_id, page=1, per_page=30, publicity='public',
-			image_sizes=['px_128x128', 'px_480mw', 'large'],
-			profile_image_sizes=['px_170x170', 'px_50x50'],
-			include_stats=True, include_sanity_level=True):
-		url = 'https://public-api.secure.pixiv.net/v1/users/%d/works.json' % (author_id)
-		params = {
-			'page': page,
-			'per_page': per_page,
-			'publicity': publicity,
-			'include_stats': include_stats,
-			'include_sanity_level': include_sanity_level,
-			'image_sizes': ','.join(image_sizes),
-			'profile_image_sizes': ','.join(profile_image_sizes),
-		}
-		r = self.auth_requests_call('GET', url, params=params)
-		return self.parse_result(r)
-
-	# 用户收藏
-	# publicity:  public, private
-	def users_favorite_works(self, author_id, page=1, per_page=30, publicity='public',
-			image_sizes=['px_128x128', 'px_480mw', 'large'],
-			profile_image_sizes=['px_170x170', 'px_50x50'],
-			include_stats=True, include_sanity_level=True):
-		url = 'https://public-api.secure.pixiv.net/v1/users/%d/favorite_works.json' % (author_id)
-		params = {
-			'page': page,
-			'per_page': per_page,
-			'publicity': publicity,
-			'include_stats': include_stats,
-			'include_sanity_level': include_sanity_level,
-			'image_sizes': ','.join(image_sizes),
-			'profile_image_sizes': ','.join(profile_image_sizes),
-		}
-		r = self.auth_requests_call('GET', url, params=params)
-		return self.parse_result(r)
-
-	# 排行榜/过去排行榜
-	# mode: [daily, weekly, monthly, male, female, rookie, daily_r18, weekly_r18, male_r18, female_r18, r18g]
-	# page: [1-n]
-	# date: '2015-04-01' (仅过去排行榜)
-	def ranking_all(self, mode='daily', page=1, per_page=50, date=None,
-			image_sizes=['px_128x128', 'px_480mw', 'large'],
-			profile_image_sizes=['px_170x170', 'px_50x50'],
-			include_stats=True, include_sanity_level=True):
-		url = 'https://public-api.secure.pixiv.net/v1/ranking/all'
-		params = {
-			'mode': mode,
-			'page': page,
-			'per_page': per_page,
-			'include_stats': include_stats,
-			'include_sanity_level': include_sanity_level,
-			'image_sizes': ','.join(image_sizes),
-			'profile_image_sizes': ','.join(profile_image_sizes),
-		}
-		if date: params['date'] = date
-		r = self.auth_requests_call('GET', url, params=params)
-		return self.parse_result(r)
-
-	# 作品搜索
-	def search_works(self, query, page=1, per_page=30, mode='text',
-			period='all', order='desc', sort='date',
-			image_sizes=['px_128x128', 'px_480mw', 'large'],
-			profile_image_sizes=['px_170x170', 'px_50x50'],
-			include_stats=True, include_sanity_level=True):
-		url = 'https://public-api.secure.pixiv.net/v1/search/works.json'
-		params = {
-			'q': query,
-			'page': page,
-			'per_page': per_page,
-			'period': period,
-			'order': order,
-			'sort': sort,
-			'mode': mode,
-			'include_stats': include_stats,
-			'include_sanity_level': include_sanity_level,
-			'image_sizes': ','.join(image_sizes),
-			'profile_image_sizes': ','.join(profile_image_sizes),
-		}
-		r = self.auth_requests_call('GET', url, params=params)
-		return self.parse_result(r)
+        img_path = os.path.join(path, name)
+        if (not os.path.exists(img_path)) or replace:
+            # Write stream to file
+            response = self.requests_call('GET', url, headers={ 'Referer': referer }, stream=True)
+            with open(img_path, 'wb') as out_file:
+                shutil.copyfileobj(response.raw, out_file)
+            del response
